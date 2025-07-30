f163c99e726a86620bad3ccbbe502966986e4553
username <drewesk@gmail.com>
2025-07-30 14:41:03 -0400

    start oauth strategies for google authentication

---
+++ src/strategies.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GitHubStrategy } from "passport-github2";
+
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const user = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        email: profile.email[0]?.value,
+        photo: profile.photos[0]?.value,
+        accessToken,
+        refreshToken,
+      };
+
+      return done(null, user);
+    }
+  )
+);
